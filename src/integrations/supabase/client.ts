0bd1c8e715fd8b77f7405cb0a87adb6386751bd6
Amit Kumar <aayamit7@gmail.com>
2025-09-10 13:21:23 +0530

    Remove .env from repo

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://qwinlyeavzanrlngpgsi.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aW5seWVhdnphbnJsbmdwZ3NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MjU2MDQsImV4cCI6MjA3MzAwMTYwNH0.nij1wsWk1aPQU5C-kl2U6EJD37_P_5dIpqMs_SKu19k";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
