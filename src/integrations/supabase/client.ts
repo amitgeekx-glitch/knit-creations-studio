dd7ff45cc871b1d2544a66e54d0a89a8316a3a34
Amit Kumar <aayamit7@gmail.com>
2025-09-10 13:29:08 +0530

    New_Page

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -8,7 +8,7 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
     storage: localStorage,
     persistSession: true,
